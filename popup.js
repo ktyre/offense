6240b2d5a35c19d4cd9b723515e173451b6f16b2
ktyre <ktyre@yammer-inc.com>
2016-01-22 23:39:34 -0800

    end of day progress

--- popup.js
+++ popup.js
@@ -1,20 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+// document.addEventListener('DOMContentLoaded', function() {
+//   var checkPageButton = document.getElementById('checkPage');
+//   checkPageButton.addEventListener('click', function() {
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
+//     var derp = $("#making-updates").val;
+//     console.log(derp);
+    
+//   }, false);
+// }, false);
 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+$("body").append("I did a jQuery thing");
